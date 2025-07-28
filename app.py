e92203d9e83333ca70751b1f18ae7c0bf7d662b3
SSohil112 <sohilkhan971170@gmail.com>
2025-07-28 11:18:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,51 +1,54 @@
 from flask import Flask, render_template, request
 import pickle
-import numpy
+import numpy as np
 
-app = Flask(__name__)
-
-# Load data and model
+# Load pickled models/data
 popular_df = pickle.load(open('popular.pkl', 'rb'))
 pt = pickle.load(open('pt.pkl', 'rb'))
 books = pickle.load(open('books.pkl', 'rb'))
 similarity_scores = pickle.load(open('similarity_scores.pkl', 'rb'))
 
+app = Flask(__name__)
+
+# Home page route
 @app.route('/')
 def index():
-    return render_template('index.html',
-                           book_name=list(popular_df['Book-Title'].values),
-                           author=list(popular_df['Book-Author'].values),
-                           image=list(popular_df['Image-URL-M'].values),
-                           votes=list(popular_df['num_ratings'].values),
-                           rating=list(popular_df['avg_rating'].values))
+    return render_template(
+        'index.html',
+        book_name=list(popular_df['Book-Title'].values),
+        author=list(popular_df['Book-Author'].values),
+        image=list(popular_df['Image-URL-M'].values),
+        votes=list(popular_df['num_rating'].values),
+        rating=list(popular_df['avg_rating'].values)
+    )
 
+# Recommendation UI page
 @app.route('/recommend')
 def recommend_ui():
     return render_template('recommend.html')
 
+# Recommendation logic
 @app.route('/recommend_books', methods=['POST'])
 def recommend():
     user_input = request.form.get('user_input')
 
-    # Book not found in dataset
-    if user_input not in pt.index:
-        return render_template('recommend.html', error=f"❌ Book '{user_input}' not found in our system. Please try another.")
+    try:
+        index = np.where(pt.index == user_input)[0][0]
+        similar_items = sorted(list(enumerate(similarity_scores[index])), key=lambda x: x[1], reverse=True)[1:9]
 
-    index = pt.index.get_loc(user_input)
-    similar_items = sorted(list(enumerate(similarity_scores[index])), key=lambda x: x[1], reverse=True)[1:5]
+        data = []
+        for i in similar_items:
+            item = []
+            temp_df = books[books['Book-Title'] == pt.index[i[0]]]
+            item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
+            item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
+            item.extend(list(temp_df.drop_duplicates('Book-Title')['Image-URL-M'].values))
+            data.append(item)
 
-    data = []
-    for i in similar_items:
-        book_title = pt.index[i[0]]
-        temp_df = books[books['Book-Title'] == book_title].drop_duplicates('Book-Title')
+        return render_template('recommend.html', data=data)
 
-        if not temp_df.empty:
-            title = temp_df['Book-Title'].values[0]
-            author = temp_df['Book-Author'].values[0]
-            image = temp_df['Image-URL-M'].values[0]
-            data.append([title, author, image])
-
-    return render_template('recommend.html', data=data)
+    except IndexError:
+        return render_template('recommend.html', data=[], error="Book not found! Try entering an exact title.")
 
 if __name__ == '__main__':
     app.run(debug=True)
